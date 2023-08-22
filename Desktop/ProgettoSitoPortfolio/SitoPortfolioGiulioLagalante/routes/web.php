36ae1dbdf1938f810f403017b482be376d69527d
Giulio Lagalante <giuliolagalante271291@gmail.com>
2023-08-22 21:44:41 +0200

    modifica Primo Selfwork

--- Desktop/ProgettoSitoPortfolio/SitoPortfolioGiulioLagalante/routes/web.php
+++ Desktop/ProgettoSitoPortfolio/SitoPortfolioGiulioLagalante/routes/web.php
@@ -16,3 +16,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/contact', function () {
+    return view('contact');
+});
+Route::get('/projects', function () {
+    return view('projects');
+});
+Route::get('/resume', function () {
+    return view('resume');
+});
+
+
